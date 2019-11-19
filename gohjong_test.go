14654f68d8b270d4a09aa5fa6c98a6fa145f391f
matsuyoshi <sfbgwm30@gmail.com>
2019-11-20 00:38:13 +0900

    add test case name

--- gohjong_test.go
+++ gohjong_test.go
@@ -7,27 +7,28 @@ import (
 
 func TestParseHand(t *testing.T) {
 	testcase := []struct {
+		name     string
 		input    string
 		expected []Tile
 	}{
-		{"", []Tile{}},
-		{"111", []Tile{}},
-		{"111m", []Tile{
+		{"01", "", []Tile{}},
+		{"02", "111", []Tile{}},
+		{"03", "111m", []Tile{
 			Tile{name: "1m", tiletype: SuitTile, num: 1},
 			Tile{name: "1m", tiletype: SuitTile, num: 1},
 			Tile{name: "1m", tiletype: SuitTile, num: 1}}},
-		{"m", nil},
+		{"04", "m", nil},
 	}
 
 	for _, tt := range testcase {
 		output, _ := ParseHand(tt.input)
 		if len(output) != len(tt.expected) {
-			t.Fatalf("Length wrong: expected %v, but got %v", tt.expected, output)
+			t.Fatalf("%s Length wrong: expected %v, but got %v", tt.name, tt.expected, output)
 		}
 
 		for idx, o := range output {
 			if o != tt.expected[idx] {
-				t.Fatalf("Element[%d] wrong: expected %v, but got %v", idx, tt.expected, output)
+				t.Fatalf("%s Element[%d] wrong: expected %v, but got %v", tt.name, idx, tt.expected, output)
 			}
 		}
 	}
@@ -35,10 +36,12 @@ func TestParseHand(t *testing.T) {
 
 func TestCheckWaiting(t *testing.T) {
 	testcase := []struct {
+		name     string
 		input    string
 		expected []OutputHand
 	}{
 		{
+			"001",
 			"1112224588899m",
 			[]OutputHand{
 				OutputHand{
@@ -51,6 +54,7 @@ func TestCheckWaiting(t *testing.T) {
 			},
 		},
 		{
+			"002",
 			"1112224688899m",
 			[]OutputHand{
 				OutputHand{
@@ -63,6 +67,7 @@ func TestCheckWaiting(t *testing.T) {
 			},
 		},
 		{
+			"003",
 			"1112223355566m",
 			[]OutputHand{
 				OutputHand{
@@ -89,6 +94,7 @@ func TestCheckWaiting(t *testing.T) {
 			},
 		},
 		{
+			"004",
 			"1113335557779m",
 			[]OutputHand{
 				OutputHand{
@@ -108,6 +114,7 @@ func TestCheckWaiting(t *testing.T) {
 			},
 		},
 		{
+			"005", // nine gates
 			"1112345678999m",
 			[]OutputHand{
 				OutputHand{
@@ -198,12 +205,12 @@ func TestCheckWaiting(t *testing.T) {
 		}
 
 		if len(tt.expected) != len(actualOutput) {
-			t.Errorf("Error: expected length %d, but got %d\n", len(tt.expected), len(actualOutput))
+			t.Errorf("Error: %s expected length %d, but got %d\n", tt.name, len(tt.expected), len(actualOutput))
 		}
 
 		if len(actualOutput) != 0 { // check slice length before check elements
 			if !reflect.DeepEqual(tt.expected, actualOutput) {
-				t.Errorf("Error: expected %v, but got %v\n", tt.expected, actualOutput)
+				t.Errorf("Error: %s expected %v, but got %v\n", tt.name, tt.expected, actualOutput)
 			}
 		}
 	}
